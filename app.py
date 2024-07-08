d86d1eaab4f34f6784288bed2ed1e2a9e70ee58b
Ignacio Cuevas <icuevas@seenka.com>
2024-07-07 21:28:58 -0300

    basic front end structure

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/get_game')
+def get_data():
+    words = [4, 5, 7, 5, 8, 4, 6]
+    syllables = 10*["ab"]
+    definitions = len(words)*["def"]
+    data = {
+        "words": words,
+        "syllables": syllables,
+        "definitions": definitions
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Your JavaScript code here
 });
+
+fetch('/api/get_game')
+    .then(response => response.json())
+    .then(data => {
+        let crossword = document.getElementById("crossword");
+        for (let i = 0; i < data.words.length; i++) {
+            let wordDiv = document.createElement("div");
+            wordDiv.classList.add("wordDiv");
+            crossword.appendChild(wordDiv);
+            for (let j = 0; j < data.words[i]; j++) {
+                let letterCell = document.createElement("div");
+                letterCell.classList.add("cell");
+                letterCell.textContent = "a";
+                letterCell.style.width = "20px";
+                letterCell.style.height = "20px";
+                wordDiv.appendChild(letterCell);
+            }
+        }
+
+        let syllables = document.getElementById("syllables");
+        for (let i = 0; i < data.syllables.length; i++) {
+            let syllableCell = document.createElement("div");
+            syllableCell.classList.add("cell");
+            syllableCell.textContent = `${data.syllables[i]}`;
+            syllableCell.style.width = "30px";
+            syllableCell.style.height = "30px";
+            syllables.appendChild(syllableCell);
+        }
+    });
